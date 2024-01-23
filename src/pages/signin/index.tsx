a7482c376cd2f9d741fde9ef5910c5704b1bc69d
yunajoe <86518113+yunajoe@users.noreply.github.com>
2024-01-23 17:37:20 +0900

    style: config에 설정된걸루 변경

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -7,10 +7,10 @@ import { useState } from 'react';
 function SignIn() {
   const [isClick, setIsClick] = useState(false);
   return (
-    <div className="w-full min-h-dvh flex justify-center items-center bg-[#FFF]">
+    <div className="w-full min-h-dvh flex-center bg-white">
       <div className="max-w-[300px] flex-1 flex flex-col items-center gap-y-10 ">
-        <div className="flex h-[64px] justify-center items-center">
-          <p className="text-[#66C57B] font-[700] text-2xl ">Read Me</p>
+        <div className="h-[64px] flex-center">
+          <p className="text-green font-bold text-2xl">Read Me</p>
         </div>
         <div className="w-full flex flex-col">
           <input placeholder="이메일" />
@@ -22,12 +22,10 @@ function SignIn() {
             type="checkbox"
             className="invisible w-[1px] h-[1px] m-[-1px]"
           />
-          <label
-            htmlFor="loginSaved "
-            className="flex justify-center items-center">
+          <label htmlFor="loginSaved " className="flex-center">
             <div
               onClick={() => setIsClick(!isClick)}
-              className={`flex justify-center items-center ${isClick ? 'bg-slate-300' : ''} rounded-full w-5 h-5 border-solid border-2  border-[#DBDBDB] cursor-pointer`}>
+              className={`flex-center ${isClick ? 'bg-slate-300' : ''} rounded-full w-5 h-5 border-solid border-2  border-[#DBDBDB] cursor-pointer`}>
               {isClick && (
                 <Image
                   alt=""
@@ -38,16 +36,16 @@ function SignIn() {
               )}
             </div>
           </label>
-          <p className="mx-2 font-[15px] text-[#363636]">로그인 상태 유지</p>
+          <p className="mx-2 font-15 text-[#363636]">로그인 상태 유지</p>
         </div>
         <RegisterButton>로그인</RegisterButton>
         <div className="flex gap-x-1">
           <p className="text-[#767676]">아이디가 없으신가요?</p>
-          <Link href="/signup" className="text-[#66C57B] font-[500]">
+          <Link href="/signup" className="text-green font-normal">
             회원가입
           </Link>
         </div>
-        <div className="flex flex-col gap-y-[20px] text-[#767676] ">
+        <div className="flex flex-col gap-y-[20px] text-[#767676]">
           <p className="text-center text-xs">SNS로 로그인/회원가입</p>
           <div className="w-[184px] flex justify-between">
             <SocialCircle />
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -4,12 +4,12 @@ import Link from 'next/link';
 
 function SignUp() {
   return (
-    <div className="w-full min-h-dvh flex justify-center items-center bg-[#FFF]">
+    <div className="w-full min-h-dvh bg-white flex-center">
       <div className="max-w-[390px] flex-1 flex flex-col items-center gap-y-10 px-[15px] ">
-        <div className="flex h-[64px] justify-center items-center">
-          <p className="text-[#66C57B] font-[700] text-2xl ">Read Me</p>
+        <div className="h-[64px] flex-center">
+          <p className="text-green font-bold text-2xl ">Read Me</p>
         </div>
-        <p className="text-[#363636] font-[700] text-xl">회원가입</p>
+        <p className="text-[#363636] font-bold text-xl">회원가입</p>
         <div className="w-full flex flex-col items-center  gap-y-[20px] text-[#767676] rounded-[10px] border-solid border-2 border-[#DBDBDB] py-5">
           <p className="text-center text-xs">SNS로 간편하게 로그인/회원가입</p>
           <div className="w-[184px] flex justify-between">
@@ -23,7 +23,7 @@ function SignUp() {
         <RegisterButton>회원가입</RegisterButton>
         <div className="flex gap-x-1">
           <p className="text-[#767676]">이미 아이디가 있으신가요?</p>
-          <Link href="/signin" className="text-[#66C57B] font-[500]">
+          <Link href="/signin" className="text-green font-normal">
             로그인
           </Link>
         </div>
