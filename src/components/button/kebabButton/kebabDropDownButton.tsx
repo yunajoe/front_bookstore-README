1b26b5821058ad1d5f9062fde83c72a1d127cb16
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-01-25 02:31:41 +0900

    feat : TODO add

--- src/components/button/kebabButton/kebabDropDownButton.tsx
+++ src/components/button/kebabButton/kebabDropDownButton.tsx
@@ -2,7 +2,7 @@ export interface KebabDropDownButtonProps {
   title1: string;
   title2: string;
 }
-
+//TODO : 디자인 및 드롭다운 펼쳐졌을때 위치 수정필요함
 function KebabDropDownButton({ title1, title2 }: KebabDropDownButtonProps) {
   return (
     <div className="flex-center flex-col absolute top-43 right-20">
