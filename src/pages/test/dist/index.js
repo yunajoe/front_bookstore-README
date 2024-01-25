d0c1a02e35941e0878005cd04cf74a8f11f89496
yunajoe <86518113+yunajoe@users.noreply.github.com>
2024-01-25 23:43:32 +0900

    style:필요없는 파일삭제

--- src/pages/test/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var DropDown_1 = require("@/components/dropDown/DropDown");
-function TestPage() {
-    return (React.createElement(React.Fragment, null,
-        React.createElement(DropDown_1["default"], null)));
-}
-exports["default"] = TestPage;
