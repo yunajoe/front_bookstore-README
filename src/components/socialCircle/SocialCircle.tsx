0f4182b6916e12b9c069e8444d23e0ab8ca708b1
yunajoe <86518113+yunajoe@users.noreply.github.com>
2024-01-25 20:47:20 +0900

    style:review반영2

--- src/components/socialCircle/SocialCircle.tsx
+++ src/components/socialCircle/SocialCircle.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function SocialCircle() {
   return (
-    <div className="w-[48px] h-[48px] rounded-full border-solid border-2 border-[#DBDBDB]" />
+    <div className="w-48 h-48 rounded-full border-solid border-2 border-[#DBDBDB]" />
   );
 }
 
