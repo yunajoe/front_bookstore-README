62a19694a6c39e3078de3697d5d82f5d022ec20c
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-01-26 17:43:57 +0900

    style : 케밥버튼 수정

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,10 +1,22 @@
+import KebabButton from '@/components/button/kebab/kebabButton';
+import CommunityCard from '@/components/card/communityCard/communityCard';
 import DropDown from '@/components/dropDown/dropDown';
+import SampleProfile from '@/public/images/SampleBookCover1.jpeg';
+import SampleBookCover from '@/public/images/SampleBookCover4.jpeg';
 
 function TestPage() {
   return (
-    <>
+    <div className="grid auto-rows-auto gap-20 p-40">  //한줄씩 밑으로 추가될거에용
       <DropDown />
-    </>
+      <CommunityCard
+        profileImg={SampleProfile}
+        userNickname="리드미"
+        createAt="2024.01.25"
+        bookCover={SampleBookCover}
+        bookTitle="책이름"
+        review="리뷰입니다"
+      />
+    </div>
   );
 }
 
