be55f4384df2899adbb557cd9b32236799141a86
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-01-26 22:38:51 +0900

    Jsx 요소아님 error,return 삼항연산자로 수정

--- src/components/button/scrollToTopButton.tsx
+++ src/components/button/scrollToTopButton.tsx
@@ -6,21 +6,21 @@ import Image from 'next/image';
 function ScrollToTopButton() {
   const [headerVisible] = useAtom(headerVisibleAtom);
 
-  const handleClickToTopButton = () => {
+  const handleClickScrollToTop = () => {
     if (!headerVisible) {
       window.scrollTo({top:0, behavior:'smooth'})
     }
   }
 
   return (
-    !headerVisible &&
-    <div onClick={handleClickToTopButton} className='cursor-pointer'>
+    headerVisible ? null :
+    <div onClick={handleClickScrollToTop} className='cursor-pointer'>
       <Image
         className="fixed right-50 bottom-80"
         src={ScrollToTopButtonImg}
         alt="페이지 상단으로 이동"
       />
-    </div>
+    </div> 
   );
 }
 
