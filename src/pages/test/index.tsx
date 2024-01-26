7965c29b99119cdc3dfdae9c2961910e0dc8d96e
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-01-26 17:57:08 +0900

    배포 에러 수정

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,12 +1,12 @@
-import KebabButton from '@/components/button/kebab/kebabButton';
 import CommunityCard from '@/components/card/communityCard/communityCard';
 import DropDown from '@/components/dropDown/dropDown';
 import SampleProfile from '@/public/images/SampleBookCover1.jpeg';
 import SampleBookCover from '@/public/images/SampleBookCover4.jpeg';
 
+//한줄씩 밑으로 추가될거에용
 function TestPage() {
   return (
-    <div className="grid auto-rows-auto gap-20 p-40">  //한줄씩 밑으로 추가될거에용
+    <div className="grid auto-rows-auto gap-20 p-40">  
       <DropDown />
       <CommunityCard
         profileImg={SampleProfile}
