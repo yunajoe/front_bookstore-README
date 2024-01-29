972d0ec18a3df242b80d669bbc8bf4736ee70e4d
yunajoe <86518113+yunajoe@users.noreply.github.com>
2024-01-29 23:20:54 +0900

    Update index.tsx

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -4,12 +4,12 @@ import TestImage1 from '@/public/images/SampleBookCover1.jpeg';
 import TestImage2 from '@/public/images/SampleBookCover3.jpeg';
 
 function TestPage() {
-//   const person1 = {
-//     id: 1,
-//     name: 'yuna',
-//     isPurchased: true,
-//     firstPurchasedDate: '2019.12.12',
-//   };
+  // const person1 = {
+  //   id: 1,
+  //   name: 'yuna',
+  //   isPurchased: true,
+  //   firstPurchasedDate: '2019.12.12',
+  // };
   // const person2 = {
   //   id: 2,
   //   name: 'yuna2',
@@ -17,9 +17,8 @@ function TestPage() {
   //   firstPurchasedDate: null,
   // };
 
-  return (  
+  return (
     <div className="flex flex-col gap-20 p-20">
-<!--       <DropDown /> -->
       <PreviewBookInfo
         title="어머 이책 사야해!"
         authorList={['이승연', '작가얌', '작가2', '작가3', '작가3', '작가3']}
@@ -42,7 +41,6 @@ function TestPage() {
         image={TestImage1}
       />
     </div>
-
   );
 }
 
