eafe0d4fc6df9b96b3e2e98986ad5dec6e5064e6
orrhrr <120437902+orrhrr@users.noreply.github.com>
2024-01-29 21:59:49 +0900

    [#42]  책 미리보기 컴포넌트 제작  (#51)
    * feat: 책 미리보기 컴포넌트 기본 레이아웃 구현
    
    * feat: 작가 두줄보이기/콤마삽입/길이초과시 ...보이기
    
    * style: 라벨 위치 수정
    
    * refactor: 동적으로 이미지 가로,세로 받을 수 있도록 수정
    
    * develop 브랜치와 머지하기 전 임시저장
    
    * refactor: 이미지 사이즈가 레이아웃보다 클 때, 라벨 위치조정되도록 수정
    
    * move: contants 파일로 상수 데이터 이동
    
    * feat: 반응형 적용(라벨은 추가 작업 필요)
    
    * remove: boerder 제거
    
    * refactor: 작가를 한줄로 표시하도록 변경
    
    * romove: 불필요한 코드 삭제
    
    * style: global font 적용 및 라벨글자 자간 적용
    
    * style: 코드 위치 변경
    
    * refactor: 이미지 로딩 상태를 추적관리 하도록 수정
    
    * remove: 불필요한 코드 제거
    
    * fix: 이미지 크기 조절되지 않는 현상 수정

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,10 +1,34 @@
 import DropDown from '@/components/dropDown/dropDown';
+import PreviewBookInfo from '@/components/book/previewBookInfo/previewBookInfo';
+import TestImage1 from '@/public/images/SampleBookCover1.jpeg';
+import TestImage2 from '@/public/images/SampleBookCover3.jpeg';
 
 function TestPage() {
   return (
-    <>
+    <div className="flex flex-col gap-20 p-20">
       <DropDown />
-    </>
+      <PreviewBookInfo
+        title="어머 이책 사야해!"
+        authorList={['이승연', '작가얌', '작가2', '작가3', '작가3', '작가3']}
+        size="lg"
+        ranking={10}
+        // image={TestImage}
+      />
+      <PreviewBookInfo
+        title="어머 이책 사야해!"
+        authorList={['이승연', '작가얌', '작가2', '작가333']}
+        size="md"
+        ranking={10}
+        image={TestImage2}
+      />
+      <PreviewBookInfo
+        title="어머 이책 사야해!"
+        authorList={['이승연', '작가얌', '작가2', '작가3']}
+        size="sm"
+        ranking={10}
+        image={TestImage1}
+      />
+    </div>
   );
 }
 
