b4f6d4fea0e6fbbe6f783db166893d50bbe37235
yunajoe <86518113+yunajoe@users.noreply.github.com>
2024-01-29 22:29:57 +0900

    style: css수정

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -34,15 +34,10 @@ function SignIn() {
   };
   return (
     <div className="w-full min-h-dvh flex-center bg-white">
-      <div className="max-w-300 flex-1 flex flex-col items-center gap-y-10">
-        <div className="h-64 flex-center mb-40 border">
-          <p className="text-green text-24 font-bold">Read Me</p>
-        </div>
-
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="w-full flex flex-col items-center gap-y-10">
-          <div className="w-full flex flex-col">
+      <div className="max-w-300 flex-1 flex flex-col items-center">
+        <p className="text-green text-24 font-bold mb-57">Read Me</p>
+        <form onSubmit={handleSubmit(onSubmit)} className="w-full">
+          <fieldset>
             <TextInput
               id="email"
               placeholder="이메일"
@@ -50,6 +45,8 @@ function SignIn() {
               required={true}
               pattern={checkEmail}
             />
+          </fieldset>
+          <fieldset>
             <PasswordInput
               id="password"
               placeholder="비밀번호"
@@ -57,41 +54,43 @@ function SignIn() {
               required={true}
               isError={errors.password}
             />
-            <SignError errors={errors} id="password" />
-          </div>
+            <div className="w-full h-20 mt-4 mb-20">
+              <SignError errors={errors} id="password" />
+            </div>
+          </fieldset>
           <div className="w-full flex">
             <input
               id="loginSaved"
               type="checkbox"
               className="invisible w-1 h-1"
             />
-            <label htmlFor="loginSaved " className="flex-center">
+            <label htmlFor="loginSaved " className="flex-center gap-x-8 mb-40">
               <div
                 onClick={() => setIsClick(!isClick)}
                 className={`flex-center ${isClick ? 'bg-green' : ''} rounded-full w-20 h-20 border-solid
                   border-2 border-gray-1 cursor-pointer`}>
                 {isClick && (
                   <Image
-                    alt=""
+                    alt="체크아이콘"
                     src="/icons/CheckIcon.svg"
                     width={10}
                     height={7}
                   />
                 )}
               </div>
+              <p className="text-15 text-black font-normal">로그인 상태 유지</p>
             </label>
-            <p className="mx-2 text-15 text-black">로그인 상태 유지</p>
           </div>
           <RegisterButton>로그인</RegisterButton>
         </form>
-        <div className="flex gap-x-1">
+        <div className="flex gap-x-4 mt-20 mb-40">
           <p className="text-gray-3">아이디가 없으신가요?</p>
           <Link href="/signup" className="text-green font-normal">
             회원가입
           </Link>
         </div>
-        <div className="flex flex-col gap-y-20">
-          <p className="text-center text-xs text-gray-3">
+        <div className="flex flex-col">
+          <p className="text-center text-12 text-gray-3 mb-20 font-normal">
             SNS로 로그인/회원가입
           </p>
           <div className="w-184 flex justify-between">
