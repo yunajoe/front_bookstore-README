09335cf9f678ec52986cf5c8aff8bccf2d626ffd
thisisthewa2 <thisisthewa2@gmail.com>
2024-01-31 02:35:27 +0900

    fix: header width 수정

--- src/components/layout/headerLayout.tsx
+++ src/components/layout/headerLayout.tsx
@@ -7,9 +7,7 @@ interface HeaderLayoutProps {
 }
 function HeaderLayout({ isLoggedIn, children }: HeaderLayoutProps) {
   return (
-    <div
-      className="min-w-fit max-w-full sticky top-0 flex-row h-90 tablet:h-170 pc:h-170 bg-white
-        z-50">
+    <div className="min-w-fit w-full sticky top-0 flex-row h-90 tablet:h-170 pc:h-170 bg-white z-50">
       {children} <NavigationTab isLoggedIn={isLoggedIn} />
     </div>
   );
--- src/components/layout/mainLayout.tsx
+++ src/components/layout/mainLayout.tsx
@@ -11,28 +11,30 @@ function MainLayout() {
   setPointVisible(isIntersecting);
 
   return (
-    <div className="flex-col justify-center">
+    <div className="flex-col items-center flex-center w-full">
       <Header isLoggedIn={true} numItemsOfCart={1} />
-      <div className="relative grid auto-rows-auto place-items-center tablet:max-w-[768px]">
+      <div className="flex-col w-full relative flex-center">
         <div className="w-300 h-20" ref={ref} />
         <div
           className="bg-green h-480 w-[1080px] mt-40 mb-87 flex-center tablet:w-[688px] tablet:mb-80
             mobile:w-330 mobile:mb-20 mx-auto">
           div두개있는 젤 위에꺼
         </div>
-        <div className="bg-gray-3 h-482 w-[1200px] flex-center mx-auto tablet:w-full mobile:w-full">
-          맞춤도서
-        </div>
-        <div className="bg-green h-[581px] w-[1200px] flex-center mx-auto tablet:w-full mobile:w-full">
-          신간도서
-        </div>
-        <div className="bg-gray-3 h-[633px] w-[1200px] flex-center mx-auto tablet:w-full mobile:w-full">
-          실시간 인기 도서
-        </div>
-        <div
-          className="bg-green h-[800px] w-[1080px] mt-120 flex-center mx-auto tablet:w-[688px]
-            mobile:w-330">
-          베스트셀러
+        <div className="tablet:max-w-[768px]">
+          <div className="bg-gray-3 h-482 w-[1200px] flex-center tablet:w-full mobile:w-full">
+            맞춤도서
+          </div>
+          <div className="bg-green h-[581px] w-[1200px] flex-center tablet:w-full mobile:w-full">
+            신간도서
+          </div>
+          <div className="bg-gray-3 h-[633px] w-[1200px] flex-center tablet:w-full mobile:w-full">
+            실시간 인기 도서
+          </div>
+          <div
+            className="bg-green h-[800px] w-[1080px] mt-120 flex-center tablet:w-[688px] mobile:w-330
+              mx-auto">
+            베스트셀러
+          </div>
         </div>
       </div>
       <ScrollToTopButton />
