7ee735f34d80707b2757dadfe83525df7f5921fa
thisisthewa2 <thisisthewa2@gmail.com>
2024-01-30 20:57:27 +0900

    feat: header dropdown 추가

--- src/components/button/header/myPageButton.tsx
+++ src/components/button/header/myPageButton.tsx
@@ -1,14 +1,45 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import IconProfile from '@/public/icons/ProfileIcon.svg';
+import KebabDropDownButton from '../kebab/kebabDropDownButton';
+import useShowDropDown from '@/hooks/useShowDropDown';
+import { MutableRefObject, useRef } from 'react';
 
 function MyPageButton() {
+  const ref = useRef() as MutableRefObject<HTMLImageElement>;
+  const [showOptions, setShowOptions] = useShowDropDown(ref, false);
+
+  const handleKebabClick = () => {
+    setShowOptions(!showOptions);
+  };
+
+  const handleMyPageClick = () => {
+    // MyPage 버튼이 클릭되었을 때 페이지로 이동
+    console.log('MyPage 버튼 클릭됨');
+  };
+
+  const handleLogoutClick = () => {
+    // 로그아웃 버튼이 클릭되었을 때 할 동작
+    console.log('로그아웃 버튼 클릭됨');
+  };
+
   return (
-    <Link href="/mypage">
-      <button className="flex relative items-center w-16 h-16 tablet:w-24 tablet:h-24 pc:w-24 pc:h-24">
-        <Image src={IconProfile} fill alt="마이페이지 버튼" />
-      </button>
-    </Link>
+    <div className="flex items-center">
+      <Image
+        src={IconProfile}
+        alt="케밥버튼"
+        onClick={handleKebabClick}
+        ref={ref}
+      />
+      {showOptions && (
+        <KebabDropDownButton
+          title1={<Link href="/mypage">마이페이지</Link>}
+          title2="로그아웃"
+          onClickTitle1={handleMyPageClick}
+          onClickTitle2={handleLogoutClick}
+        />
+      )}
+    </div>
   );
 }
 
