ca6a66ac7cbc223cf5e2d784a0d922f3b075fb50
orrhrr <120437902+orrhrr@users.noreply.github.com>
2024-01-30 16:54:18 +0900

    [#62]  책 미리보기 컴포넌트 라벨색변경 기능+가격표시 추가  (#66)
    * refactor: 카테고리, 가격 표시
    
    * refactor: [라벨]10위 이상은 회색표시/세자리수 일때 숫자위치 조정
    
    * style: 순위표시 위치 미세조정

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -4,19 +4,6 @@ import TestImage1 from '@/public/images/SampleBookCover1.jpeg';
 import TestImage2 from '@/public/images/SampleBookCover3.jpeg';
 
 function TestPage() {
-  // const person1 = {
-  //   id: 1,
-  //   name: 'yuna',
-  //   isPurchased: true,
-  //   firstPurchasedDate: '2019.12.12',
-  // };
-  // const person2 = {
-  //   id: 2,
-  //   name: 'yuna2',
-  //   isPurchased: false,
-  //   firstPurchasedDate: null,
-  // };
-
   return (
     <div className="flex flex-col gap-20 p-20">
       <PreviewBookInfo
@@ -24,20 +11,21 @@ function TestPage() {
         authorList={['이승연', '작가얌', '작가2', '작가3', '작가3', '작가3']}
         size="lg"
         ranking={10}
-        // image={TestImage}
+        category="가정/육아"
+        price={123456789}
       />
       <PreviewBookInfo
         title="어머 이책 사야해!"
         authorList={['이승연', '작가얌', '작가2', '작가333']}
         size="md"
-        ranking={10}
+        ranking={30}
         image={TestImage2}
       />
       <PreviewBookInfo
-        title="어머 이책 사야해!"
+        title="겁나 비싼 책"
         authorList={['이승연', '작가얌', '작가2', '작가3']}
         size="sm"
-        ranking={10}
+        ranking={100}
         image={TestImage1}
       />
     </div>
