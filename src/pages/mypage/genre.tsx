c6ec3c4558a13e59e8f4d4b08e98fc63f4129982
thisisthewa2 <thisisthewa2@gmail.com>
2024-01-31 03:31:31 +0900

    feat: editToggleButton 컴포넌트화

--- src/pages/mypage/genre.tsx
+++ src/pages/mypage/genre.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import GenreButton from '@/components/button/genreButton';
 import { ReadMeGenreList } from '../api/mock';
+import EditToggleButton from '@/components/button/editToggleButton';
 
 export default function Genre() {
   const [isEditMode, setEditMode] = useState(false);
@@ -14,11 +15,9 @@ export default function Genre() {
     return 'pc:grid pc:grid-cols-10 pc:gap-4 tablet:grid tablet:grid-cols-6 tablet:gap-4 mobile:grid mobile:grid-cols-3 mobile:gap-4';
   };
   return (
-    <div className="flex-col">
-      <div>선호장르 선택</div>
-      <button onClick={handleEditModeToggle}>
-        {isEditMode ? '수정 완료' : '수정하기'}
-      </button>
+    <div className="flex-col flex-center">
+      <div className="text-20 font-bold">선호장르 선택</div>
+
       <div className={`flex flex-wrap ${getButtonLayoutClass()}`}>
         {genres.map((genre, index) => (
           <GenreButton
@@ -29,6 +28,10 @@ export default function Genre() {
           />
         ))}
       </div>
+      <EditToggleButton
+        isEditMode={isEditMode}
+        onClick={handleEditModeToggle}
+      />
     </div>
   );
 }
