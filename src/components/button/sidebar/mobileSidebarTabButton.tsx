3d8a241806dcc1ba9cb3e057bd84b7fe6c273751
HellenHong <hongjw030@naver.com>
2024-02-01 04:17:02 +0900

    design: 화살표 버튼에 디자인 추가

--- src/components/button/sidebar/mobileSidebarTabButton.tsx
+++ src/components/button/sidebar/mobileSidebarTabButton.tsx
@@ -25,12 +25,14 @@ function MobileSidebarTabButton({
         onClick={handleClick}
         className="pc:hidden tablet:hidden mobile:flex-center mobile:gap-4 text-[13px]">
         {location ?? '전체보기'}
-        <Image
-          src={`${showOptions ? '/icons/UpArrow.svg' : 'icons/DownArrow.svg'}`}
-          alt=""
-          width={19}
-          height={19}
-        />
+        <div className="w-19 h-19 border-[1px] rounded-[100%] border-gray-1 overflow-hidden">
+          <Image
+            src={`${showOptions ? '/icons/UpArrow.svg' : 'icons/DownArrow.svg'}`}
+            alt="카테고리 펼치기 버튼"
+            width={19}
+            height={19}
+          />
+        </div>
       </button>
       {(showOptions || env !== 'mobile') && (
         <SidebarTab isDomestic={isDomestic} location={location} />
