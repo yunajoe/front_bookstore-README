6fe799cf8934127c0ea7cf4bb46a2ed933dd2b73
thisisthewa2 <thisisthewa2@gmail.com>
2024-01-31 20:09:09 +0900

    fix: 장바구니 아이템이 99개 이상일 경우 예외처리

--- src/components/button/header/cartButton.tsx
+++ src/components/button/header/cartButton.tsx
@@ -1,12 +1,19 @@
 import Image from 'next/image';
 import CartIcon from '@/public/icons/CartIcon.svg';
 import Link from 'next/link';
+import { getNumItemsOfCart } from '@/utils/getNumItemsOfCart';
 
 interface CardChipProps {
   numItemsOfCart: number;
 }
 
 function CartButton({ numItemsOfCart }: CardChipProps) {
+  const items = getNumItemsOfCart(numItemsOfCart);
+  const getCircleStyle = () =>
+    Number(items) < 10
+      ? 'text-12 mobile:text-10'
+      : 'text-[9px] mobile:text-[7px]';
+
   return (
     <Link href="/cart">
       <button className="flex relative items-center w-14 h-14 tablet:w-24 tablet:h-24 pc:w-24 pc:h-24">
@@ -14,11 +21,10 @@ function CartButton({ numItemsOfCart }: CardChipProps) {
           <Image src={CartIcon} alt="장바구니 이미지" fill />
           {numItemsOfCart && (
             <div
-              className="absolute bottom-8 pc:bottom-12 tablet:bottom-12 left-10 tablet:left-17
-                pc:left-17 bg-green text-white rounded-full p-1 text-10 tablet:text-12
-                pc:text-12 w-14 h-14 tablet:w-16 tablet:h-16 pc:w-16 pc:h-16 flex items-center
-                justify-center">
-              {numItemsOfCart}
+              className={`absolute bottom-6 pc:bottom-12 tablet:bottom-12 left-8 tablet:left-17 pc:left-17
+                bg-green text-white rounded-full p-1 mobile:w-14 mobile:h-14 w-20 h-20 flex
+                items-center justify-center ${getCircleStyle()}`}>
+              {items}
             </div>
           )}
         </div>
