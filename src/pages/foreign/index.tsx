ef2d71187ea5c942f7de8f84f26e821f6fc5f191
HellenHong <hongjw030@naver.com>
2024-02-02 16:02:03 +0900

    styles: 일부 글자 수정

--- src/pages/foreign/index.tsx
+++ src/pages/foreign/index.tsx
@@ -18,35 +18,29 @@ export default function ForeignPage() {
         </article>
         <Spacing height={[60, 40, 40]} />
         <article className="flex flex-col gap-50 tablet:gap-40 mobile:gap-20">
-          <h1 className="text-black text-20">신간도서</h1>
+          <h1 className="text-black text-20">신간도서 넣을 곳</h1>
           <div
             role="temp"
             className="bg-gray-1 w-[895px] h-[334px] tablet:w-[511px] tablet:h-[336px] mobile:w-[330px]
-              mobile:h-[297px]">
-            캐러셀 넣을 곳!!!!!
-          </div>
+              mobile:h-[297px]"></div>
         </article>
         <Spacing height={[120, 80, 80]} />
         <article className="flex flex-col gap-50 tablet:gap-40 mobile:gap-20">
-          <h1 className="text-black text-20">베스트셀러</h1>
+          <h1 className="text-black text-20">베스트셀러 넣을 곳</h1>
           <div
             role="temp"
             className="bg-gray-1 w-[895px] h-[708px] tablet:w-[511px] tablet:h-[1464px]
-              mobile:w-[330px] mobile:h-[1735px]">
-            그냥 공백~!!! 나중에 grid로 채우면 될듯~!!
-          </div>
+              mobile:w-[330px] mobile:h-[1735px]"></div>
         </article>
         <Spacing height={[120, 80, 80]} />
         <article className="flex flex-col gap-50 tablet:gap-40 mobile:gap-20">
           <div className="flex justify-between items-center">
-            <h1 className="text-black text-20">모든 도서</h1>
+            <h1 className="text-black text-20">모든 도서 넣을 곳</h1>
           </div>
           <div
             role="temp"
             className="bg-gray-1 w-[895px] h-[708px] tablet:w-[511px] tablet:h-[1464px]
-              mobile:w-[330px] mobile:h-[1735px]">
-            그냥 공백~!! 요기도 grid로 채우고 무한스크롤 붙이면 될듯~!!!
-          </div>
+              mobile:w-[330px] mobile:h-[1735px]"></div>
         </article>
       </>
     </SidebarLayout>
