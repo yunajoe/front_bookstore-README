ae9d272a297edef88ebd1f25051545b8761dd23d
thisisthewa2 <thisisthewa2@gmail.com>
2024-02-07 03:54:37 +0900

    style: tab button 스타일 수정

--- src/components/button/header/TabButton.tsx
+++ src/components/button/header/TabButton.tsx
@@ -9,9 +9,15 @@ interface TabButtonProps {
 
 function TabButton({ selected, onClick, title, isSmall }: TabButtonProps) {
   const fontStyle = `${isSmall ? 'text-16 mobile:text-12' : 'text-18 mobile:text-14'}`;
+  const selectedStyle =
+    selected && isSmall
+      ? 'border border-b-2 border-x-transparent border-b-green border-t-white text-green'
+      : selected
+        ? 'text-green'
+        : '';
   return (
     <button
-      className={`${fontStyle} font-bold ${selected ? 'text-green' : 'text-black'}`}
+      className={`flex-center mt-1 h-68 mobile:h-48 ${fontStyle} font-bold ${selectedStyle}`}
       onClick={onClick}>
       {title}
     </button>
