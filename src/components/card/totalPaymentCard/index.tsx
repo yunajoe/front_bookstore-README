afe3963295412e896e2d7597344c0dfb281c24c6
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-02-11 23:38:04 +0900

    feat : termsCheckbox props 추가 및 totalPriceCard연결

--- src/components/card/totalPaymentCard/index.tsx
+++ src/components/card/totalPaymentCard/index.tsx
@@ -1,15 +1,16 @@
 import TermsCheckbox from "@/components/container/terms/terms";
-import TotalPrice from "./totalPrice"
+import TotalPrice from "@/components/card/totalPaymentCard/totalPrice"
 import RegisterButton from "@/components/button/register/registerButton";
+import { REQUIRED_FOR_PAYMENT } from "src/constants/sign";
 
 function TotalPriceCard() {
   return (
-    <div>
+    <div className="flex flex-col w-432 gap-20">
       <TotalPrice title="총 상풒 금액" price="23500" />
       <TotalPrice title="총 배송비" price="+3000" />
       <TotalPrice title="총 할일 금액" price="-3000" />
       <TotalPrice title="결제 금액" price="23500" />
-      <TermsCheckbox />
+      <TermsCheckbox entire="전체동의(필수)" checkContent={REQUIRED_FOR_PAYMENT} useFormContextProps={false} showLastButton={true} />
       <RegisterButton>23500원 결제하기</RegisterButton>
     </div>
   );
