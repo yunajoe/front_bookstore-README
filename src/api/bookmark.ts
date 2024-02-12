44efa69fd9d5b6afed2dba5e4de2a9f29a184470
yunajoe <86518113+yunajoe@users.noreply.github.com>
2024-02-12 15:51:26 +0900

    api추가

--- src/api/bookmark.ts
+++ src/api/bookmark.ts
@@ -5,3 +5,11 @@ export const getCart = async (id: number) => {
   const result = await instance.get(`review/${id}`);
   return result.data;
 };
+
+
+// 해당 userID로 찜 list조회
+
+export const getBookMarkList = async () => {
+  // http://15.165.141.22:8080/bookmark?memberId=3&offset=1&limit=10&sort=price
+
+}
