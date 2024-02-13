2e4e9e8f49900f447087142f9409da5853ab4c65
yunajoe <86518113+yunajoe@users.noreply.github.com>
2024-02-13 09:54:50 +0900

    cartPageAPI

--- src/api/cart.ts
+++ src/api/cart.ts
@@ -20,3 +20,18 @@ export const deleteReview = async (id: number) => {
   const result = await instance.delete(`review/${id}`);
   return result.data;
 };
+
+
+// 해당user의 장바구니 조회 
+export const getBasketList = async (memberId: number) => {
+  const result = await instance.get(`basket/${memberId}`);
+  return result.data;
+};
+
+
+// http://15.165.141.22:8080/basket?basketIds=1%2C2%2C3
+// 해당 user의 장바구니에 들어있는 아이템 삭제  
+export const deleteBasket = async (basketId: number[]) => {
+   return await instance.delete(`basket/${basketId}`);
+ 
+};
