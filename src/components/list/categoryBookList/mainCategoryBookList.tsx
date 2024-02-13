7c5827f157723005e2e7597ae128535b054cd0b3
HellenHong <hongjw030@naver.com>
2024-02-13 18:48:39 +0900

    feat: 무한스크롤 훅 생성

--- src/components/list/categoryBookList/mainCategoryBookList.tsx
+++ src/components/list/categoryBookList/mainCategoryBookList.tsx
@@ -1,6 +1,5 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Link from "next/link";
-import { useInfiniteQuery } from "@tanstack/react-query";
 import { useAtom } from "jotai";
 
 import { getBook } from "@/api/book";
@@ -8,55 +7,49 @@ import PreviewBookInfo from "@/components/book/previewBookInfo/previewBookInfo";
 import DropDown from "@/components/dropDown/dropDown";
 import useCarouselEnv from "@/hooks/useCarouselEnv";
 import useInfinite from "@/hooks/useInfinite";
-import { BookParams } from "@/types/api/book";
+import useCustomInfiniteQuery from "@/hooks/useCustomInfiniteQuery";
 import { LocatedCategoryAtom } from "@/store/state";
 
 import { BOOK_OLDER_STANDARD } from "src/constants/orderList";
-import TestImage1 from '@/public/images/SampleBookCover1.jpeg';
+import SkeletonPreviewBookInfo from "@/components/skeleton/previewBookInfo/skeleton";
 
-const INITIAL_QUERYPARAMS = {
-  bookId: "1",
-  limit: "10",
+const CURRENT_ORDER = {
   sort: "VIEW",
-  ascending: "false",
+  ascending: false,
 }
 
 function MainCategoryBookList() {
-  const [selectedOrder, setSelectedOrder] = useState("조회순");
   const { env } = useCarouselEnv();
   const [locatedCategory,] = useAtom(LocatedCategoryAtom);
-  const [queryParams, setQueryParams] = useState<BookParams>(INITIAL_QUERYPARAMS);
+  const [selectedOrder, setSelectedOrder] = useState("조회순");
+  const [currentOrder, setCurrentOrder] = useState(CURRENT_ORDER);
   const [apiRef, isIntersecting] = useInfinite();
 
-  const {
-    fetchNextPage,
-    isFetchingNextPage, isLoading,
-    data
-  } = useInfiniteQuery({
-    queryKey: [selectedOrder, locatedCategory.mainId, "main-category-book-list"],
-    queryFn: ({pageParam = 1}) => {
-      return getBook({ endpoint: `${locatedCategory.mainId}/main`, params: { ...queryParams, bookId: String(pageParam) } })
+  const { data, isFetchingNextPage, hasNextPage } = useCustomInfiniteQuery({
+    endpoint: `${locatedCategory.mainId}/main`,
+    queryKey: [selectedOrder, String(locatedCategory.mainId), "main-category-book-list"],
+    queryFunc: getBook,
+    cursorName: "bookId",
+    sort: currentOrder.sort,
+    ascending: currentOrder.ascending,
+    refetchTrigger: (isIntersecting),
+    getNextPageParamsFunc:  (lastPage) =>  lastPage?.data.books.length <= 1 || lastPage?.data.cursorId <= 0 ? undefined : lastPage.data.books[lastPage.data.books.length - 1].bookId,
+    selectFunc: (data) => {
+      return (data.pages ?? []).flatMap(page => {
+        if (page?.data?.books.length < 2) return page?.data?.books;
+        return page?.data?.books.slice(0, page.data.books.length - 1)
+      })
     },
-    getNextPageParam: (lastPage, allPages) =>  lastPage?.data.books.length <= 0 || lastPage?.data.cursorId < 0 ? undefined : lastPage.data.books[lastPage.data.books.length - 1].bookId ,
-    initialPageParam: 1,
-    select: (data) => (data.pages ?? [] ).flatMap(page => page?.data?.books.slice(0, page.data.books.length -1))
   })
-
-
-  useEffect(() => {
-    if (isIntersecting && !isFetchingNextPage) {
-      fetchNextPage();
-    }
-  }, [isIntersecting])
 
   const onSelectedOrder = (menu: string) => {
     setSelectedOrder(menu);
-    if (menu === "조회순") setQueryParams((prev) => { return { ...prev, sort: "VIEW", ascending: "false" } });
-    if (menu === "신상품순") setQueryParams((prev) => { return { ...prev, sort: "NEWEST", ascending: "false" } });
-    if (menu === "별점순") setQueryParams((prev) => { return { ...prev, sort: "STAR", ascending: "false" } });
-    if (menu === "리뷰 많은순") setQueryParams((prev) => { return { ...prev, sort: "REVIEW", ascending: "false" } });
-    if (menu === "낮은 가격순") setQueryParams((prev) => { return { ...prev, sort: "PRICE", ascending: "true" } });
-    if (menu === "높은 가격순") setQueryParams((prev) => { return { ...prev, sort: "PRICE", ascending: "false" } });
+    if (menu === "조회순") setCurrentOrder((prev) => { return { ...prev, sort: "VIEW", ascending: false } });
+    if (menu === "신상품순") setCurrentOrder((prev) => { return { ...prev, sort: "NEWEST", ascending: false } });
+    if (menu === "별점순") setCurrentOrder((prev) => { return { ...prev, sort: "STAR", ascending: false } });
+    if (menu === "리뷰 많은순") setCurrentOrder((prev) => { return { ...prev, sort: "REVIEW", ascending: false } });
+    if (menu === "낮은 가격순") setCurrentOrder((prev) => { return { ...prev, sort: "PRICE", ascending: true } });
+    if (menu === "높은 가격순") setCurrentOrder((prev) => { return { ...prev, sort: "PRICE", ascending: false } });
   }
 
   return (
@@ -75,7 +68,7 @@ function MainCategoryBookList() {
           return (
             <Link href={`/bookdetail/${book.bookId}`} key={book.bookId} className="w-fit h-fit">
             <PreviewBookInfo
-              image={book.bookImgUrl ?? TestImage1}
+              image={book.bookImgUrl}
               title={book.bookTitle}
               authorList={book.authors}
               price={book.price}
@@ -86,10 +79,16 @@ function MainCategoryBookList() {
         })}
         
       </div>
-      {(isFetchingNextPage || isLoading || isIntersecting) && 
-      <div className="h-[300] w-full"></div>
+      {(isFetchingNextPage || isIntersecting) && 
+        <div
+          className="min-h-400 w-[895px] mobile:w-[330px]
+           tablet:w-[511px] grid grid-flow-row grid-cols-5 tablet:grid-cols-3 mobile:grid-cols-2 auto-rows-auto gap-x-20 gap-y-40 mobile:gap-y-0 ">          {[0, 1, 2].map(el => {
+            return <SkeletonPreviewBookInfo key={el} />
+          })
+        }
+        </div>
       }
-      <div className="h-15 w-300" ref={apiRef} ></div>
+        <div className={`h-5 w-300 ${hasNextPage ? "block" : "hidden"}`} ref={apiRef} ></div>
 
     </article>
   )}
