9aed7ee710afaa335492c665213a46cbf7376a40
yunajoe <86518113+yunajoe@users.noreply.github.com>
2024-02-13 10:04:43 +0900

    temp로 jwt붙임

--- src/libs/instance.ts
+++ src/libs/instance.ts
@@ -5,5 +5,6 @@ export const instance = axios.create({
   headers: {
     "Content-Type": "application/json",
   }
-})
+})  
 
+instance.defaults.headers.common["Authorization"] = `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBQ0NFU1NfVE9LRU4iLCJpYXQiOjE3MDc3ODU3OTgsImV4cCI6MTcwNzc4NzU5OCwiZW1haWwiOiJ5dW5ham9lQGdtYWlsLmNvbSJ9.Qwk-XoGdFAJilBP0TEaRkUfUzPl4IpSTnMyhvrJbxtx8eMWCLq-XpKLbqXUpgglHDS_HVBn9lswlfaYp3yRL1Q`
