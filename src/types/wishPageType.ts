00de4b5d821a96275b16a30a1012c46611cf7dcf
yunajoe <86518113+yunajoe@users.noreply.github.com>
2024-02-13 23:40:45 +0900

    파일삭제

--- src/types/wishPageType.ts
+++ src/types/wishPageType.ts
@@ -18,3 +18,15 @@ export type BookMarkListData = {
   bookmarkId: number
 }
 
+
+
+export type WishListData = {
+  id: number;
+  image: string;
+  title: string;
+  author: string;
+  rating: number;
+  genre: string;
+  price: number;
+  clicked?: number;
+};
