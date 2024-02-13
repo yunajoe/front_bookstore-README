8afd3def00a5ac25569d383ed1ff96f3c8225724
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-02-13 15:35:59 +0900

    refactor : category api 수정

--- src/api/category.ts
+++ src/api/category.ts
@@ -1,13 +1,19 @@
 import { instance } from 'src/libs/instance';
 
-//국내, 해외 카테고리 조회
-export const getCategory = async (category:string) => {
-  const result = await instance.get(`review/${category}`);
-  return result.data;
-};
-
 // 전체 카테고리 리스트 조회
 export const getCategoryList = async () => {
   const result = await instance.get(`/category`);
   return result.data.data;
 };
+
+//국내, 해외 카테고리 조회
+export const getCategory = async (category:string) => {
+  const result = await instance.get(`category/${category}`);
+  return result.data;
+};
+
+//국내 및 해외 하위 카테고리 조회
+export const getSubCategory = async (mainId: number, subId: number) => {
+  const result = await instance.get(`category/${mainId}/${subId}`);
+  return result.data;
+}
