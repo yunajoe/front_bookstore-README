9623621eec8551dc73109b7f0b12182fb1440645
HellenHong <hongjw030@naver.com>
2024-02-16 22:14:37 +0900

    feat: 데이터가 없을 때 보여줄 문구 추가

--- src/pages/domestic/[category]/index.tsx
+++ src/pages/domestic/[category]/index.tsx
@@ -27,21 +27,27 @@ function CategoryPage() {
         eventSizeClassName="w-[340px] h-[483px] tablet:w-194 tablet:h-275 mobile:w-330 mobile:h-90"
       />
       <Spacing height={[60, 40, 40]} />
-      {data ? (
-        <CategoryCarousel data={data?.data.books} responsive={responsive} />
-      ) : null}
-      <Spacing height={[120, 80, 80]} />
+      {data && data?.data.books.length > 0 ? (
+        <>
+          <CategoryCarousel data={data?.data.books} responsive={responsive} />
+          <Spacing height={[120, 80, 80]} />
 
-      <article className="flex flex-col gap-50 mobile:gap-20 tablet:gap-40">
-        <h1 className="text-20 text-black">베스트셀러</h1>
-        <div
-          role="temp"
-          className="h-[500px] w-[895px] bg-gray-1 mobile:w-[330px]
+          <article className="flex flex-col gap-50 mobile:gap-20 tablet:gap-40">
+            <h1 className="text-20 text-black">베스트셀러</h1>
+            <div
+              role="temp"
+              className="h-[500px] w-[895px] bg-gray-1 mobile:w-[330px]
             tablet:w-[511px]"></div>
-      </article>
-      <Spacing height={[120, 80, 80]} />
+          </article>
+          <Spacing height={[120, 80, 80]} />
 
-      <SubCategoryBookList />
+          <SubCategoryBookList />
+        </>
+      ) : (
+        <div className="flex-center w-full pt-120 text-gray-4">
+          이 카테고리에 등록된 도서가 없어요!
+        </div>
+      )}
     </SidebarLayout>
   );
 }
