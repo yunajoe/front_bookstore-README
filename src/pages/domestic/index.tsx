99f358b8ad45c4f27522db6866369951f6bbe7ca
HellenHong <hongjw030@naver.com>
2024-02-16 21:57:23 +0900

    feat: 카테고리 페이지 캐러셀에 데이터 연결

--- src/pages/domestic/index.tsx
+++ src/pages/domestic/index.tsx
@@ -1,16 +1,23 @@
 /** 카테고리 페이지 > 국내 > 전체*/
 
+import { useGetBook } from '@/api/book';
 import SidebarLayout from '@/components/layout/sidebarLayout';
 import Spacing from '@/components/container/spacing/spacing';
 import CategoryCarousel from '@/components/carousel/categoryCarousel';
 import MainCategoryBookList from '@/components/container/categoryBookList/mainCategoryBookList';
 import EventSection from '@/components/container/eventSection/eventSection';
-
+import { useCategoryCarouselParams } from '@/hooks/useInitialParams';
 import { responsive } from '@/utils/checkResponsiveEnv';
 
-import { carouselMockData } from '../api/mock/carouselMock';
-
 export default function DomesticPage() {
+  const INITIAL_PARAMS = useCategoryCarouselParams();
+  const { data } = useGetBook({
+    endpoint: '0/main',
+    params: {
+      ...INITIAL_PARAMS,
+    },
+  });
+
   return (
     <SidebarLayout>
       <Spacing height={[0, 0, 20]} />
@@ -20,8 +27,9 @@ export default function DomesticPage() {
         eventSizeClassName="w-[340px] h-[483px] tablet:w-194 tablet:h-275 mobile:w-330 mobile:h-90"
       />
       <Spacing height={[60, 40, 40]} />
-
-      <CategoryCarousel data={carouselMockData} responsive={responsive} />
+      {data ? (
+        <CategoryCarousel data={data?.data.books} responsive={responsive} />
+      ) : null}
       <Spacing height={[120, 80, 80]} />
 
       <article className="flex flex-col gap-50 mobile:gap-20 tablet:gap-40">
