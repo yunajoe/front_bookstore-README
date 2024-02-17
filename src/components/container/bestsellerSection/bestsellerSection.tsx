55a50765189862a18e3984681843af65fd4f6815
thisisthewa2 <thisisthewa2@gmail.com>
2024-02-17 20:39:54 +0900

    style: 여백 조정

--- src/components/container/bestsellerSection/bestsellerSection.tsx
+++ src/components/container/bestsellerSection/bestsellerSection.tsx
@@ -9,11 +9,11 @@ interface BestSellerSectionProps {
 
 function BestSellerSection({ page, bookList }: BestSellerSectionProps) {
   // page 값이 'main'이면 mx 값을 표시해주고, 아니면 제거합니다.
-  const mxClass = page === 'main' ? 'tablet:mx-40' : '';
+  const mxClass = page === 'main' ? 'tablet:mx-40 mobile:mx-15' : '';
 
   return (
     <div
-      className={`flex-col mobile:mx-15 mobile:my-80 mobile:w-330 ${mxClass} tablet:my-120
+      className={`flex-col  mobile:my-80 mobile:w-330 ${mxClass} tablet:my-120
         tablet:w-[690px] pc:my-120 pc:w-[1080px]`}>
       <div className="flex justify-between mobile:mb-20 tablet:mb-40 pc:mb-50">
         <div className="text-20 font-bold">베스트셀러</div>
@@ -21,7 +21,7 @@ function BestSellerSection({ page, bookList }: BestSellerSectionProps) {
           더보기
         </Link>
       </div>
-      <div className="flex-center flex flex-wrap gap-x-10 gap-y-62 tablet:hidden pc:gap-x-30">
+      <div className="flex-center flex flex-wrap justify-start gap-x-10 gap-y-62 tablet:hidden pc:gap-x-30">
         {bookList?.map((book, index) => (
           <PreviewBookInfo
             key={book.bookId}
