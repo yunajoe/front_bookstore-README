3f50ffc49c9cfbaefd522f11713a13055e7e373b
HellenHong <hongjw030@naver.com>
2024-02-20 20:20:26 +0900

    refactor: 중복호출 방지를 위한 isPending 활용

--- src/hooks/useClickLikeButton.ts
+++ src/hooks/useClickLikeButton.ts
@@ -1,30 +1,39 @@
-import { useQueryClient } from "@tanstack/react-query";
+import { useQueryClient  } from "@tanstack/react-query";
 import { useState } from "react";
 
 import { useDeleteBookmark, usePostBookmark } from "@/api/bookmark";
 
 function useClickLikeButton({ bookId = "-1", bookmarkId = -1, isBookmarked = false }) {
-  //옵티미스틱 상태
+
+  // 옵티미스틱 상태
   const [bookmarkState, setBookmarkState] = useState(isBookmarked);
   const queryClient = useQueryClient();
-  
+
   // 북마크하는 함수
-  const { mutate: handleCheckBookmarkMutate } = usePostBookmark({
+  const { mutate: handleCheckBookmarkMutate, isPending } = usePostBookmark({
     bookId: Number(bookId),
+    onMutate: () => {
+      queryClient.cancelQueries();
+    },
     onSettle: () => {
-      queryClient.invalidateQueries({ queryKey: ['bookmark', bookId] });
+      queryClient.invalidateQueries({ queryKey: ['bookmark', String(bookId)] });
     },
   });
   
   // 북마크 삭제하는 함수
   const { mutate: handleRemoveBookmarkMutate } = useDeleteBookmark({
     bookmarkId: String(bookmarkId),
+    onMutate: () => {
+      queryClient.cancelQueries();
+    },
     onSettle: () => {
-      queryClient.invalidateQueries({ queryKey: ['bookmark', bookId] });
+      queryClient.invalidateQueries({ queryKey: ['bookmark', String(bookId)] });
     },
   });
 
   const handleBookmarkClick = () => {
+    if (isPending) return;
+
     setBookmarkState(!isBookmarked);
     if (isBookmarked) {
       handleRemoveBookmarkMutate();
