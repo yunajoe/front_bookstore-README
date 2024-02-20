539a084cbe0e25a91eb785e57f43dc0129cb7e8e
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-02-20 15:03:45 +0900

    feat: 글쓰기 모달 연결

--- src/components/button/addcommunityButton.tsx
+++ src/components/button/addcommunityButton.tsx
@@ -1,22 +1,28 @@
 import AddCommunityButtonImg from '@/public/icons/AddCommunityButton.svg';
-import { pointVisibleAtom } from '@/store/state';
-import { useAtom } from 'jotai';
 import Image from 'next/image';
+import { useState } from 'react';
+import AddCommunityCard from '../modal/addCommunityCard';
 
 function AddCommunityButton() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleAddCommunityButtonClick = () => {
-    
+    setIsModalOpen(!isModalOpen);
   };
 
   return (
-    <div onClick={handleAddCommunityButtonClick} className="pc:hidden tablet:hidden mobile:fixed mobile:bottom-60 mobile:right-15 cursor-pointer">
-      <Image
-        className="fixed bottom-80 right-20 mobile:bottom-124 mobile:right-20"
-        src={AddCommunityButtonImg}
-        alt="글쓰기 모달로 이동"
-      />
-    </div>
+    <>
+      <div
+        onClick={handleAddCommunityButtonClick}
+        className="cursor-pointer mobile:fixed tablet:hidden pc:hidden">
+        <Image
+          className="fixed mobile:bottom-60 mobile:right-15"
+          src={AddCommunityButtonImg}
+          alt="글쓰기 모달로 이동"
+        />
+      </div>
+      {isModalOpen && <AddCommunityCard onClick={handleAddCommunityButtonClick} />}
+    </>
   );
 }
 
