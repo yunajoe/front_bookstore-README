92808763068c112323f454bace68bf475c6fce4f
HellenHong <hongjw030@naver.com>
2024-02-20 16:58:56 +0900

    style: 글자색 일부 수정

--- src/components/book/bookInformation/bookInformation.tsx
+++ src/components/book/bookInformation/bookInformation.tsx
@@ -4,7 +4,7 @@
 function BookInformation() {
   return (
     <section className="flex flex-col gap-20">
-      <h2 className="text-gray-6 text-[22px] font-bold">상품 정보</h2>
+      <h2 className="text-[22px] font-bold text-gray-4">상품 정보</h2>
       <div className="max-h-792px min-h-[589px] w-full min-w-330 max-w-[710px] bg-gray-5">
         상품 정보를 넣을 곳.
       </div>
