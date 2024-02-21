e4dac404f26e08b698585dfc34e7fb7466ba62d8
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-02-22 01:20:42 +0900

    feat : kebabDropDownButton 수정

--- src/components/button/kebab/kebabButton.tsx
+++ src/components/button/kebab/kebabButton.tsx
@@ -1,21 +1,19 @@
 import Image from 'next/image';
 import KebabImg from '@/public/icons/Kebab.svg';
-import { MutableRefObject, useRef, useState } from 'react';
+import { MutableRefObject, useRef } from 'react';
 import useShowDropDown from '@/hooks/useShowDropDown';
 import KebabDropDownButton, {
   KebabDropDownButtonProps,
 } from '@/components/button/kebab/kebabDropDownButton';
-import AlertModal from '@/components/modal/alertModal';
-import { ALERT_MODAL } from '@/constants/alertModal';
 
-function KebabButton({ title1, title2, id }: KebabDropDownButtonProps) {
+function KebabButton({
+  title1,
+  title2,
+  onClickTitle1,
+  onClickTitle2,
+}: KebabDropDownButtonProps) {
   const ref = useRef() as MutableRefObject<HTMLImageElement>;
   const [showOptions, setShowOptions] = useShowDropDown(ref, false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleAlertModalOpenClick = () => {
-    setIsModalOpen(!isModalOpen);
-  };
 
   const handleKebabClick = () => {
     setShowOptions(!showOptions);
@@ -34,15 +32,8 @@ function KebabButton({ title1, title2, id }: KebabDropDownButtonProps) {
         <KebabDropDownButton
           title1={title1}
           title2={title2}
-          onClickTitle2={handleAlertModalOpenClick}
-        />
-      )}
-      {isModalOpen && (
-        <AlertModal
-          title={ALERT_MODAL[title2 as string].title}
-          description={ALERT_MODAL[title2 as string].description}
-          onClick={handleAlertModalOpenClick}
-          id={id}
+          onClickTitle1={onClickTitle1}
+          onClickTitle2={onClickTitle2}
         />
       )}
     </>
--- src/components/card/communityCard/communityCard.tsx
+++ src/components/card/communityCard/communityCard.tsx
@@ -3,6 +3,9 @@ import EmojiButtonContainer from '@/components/card/communityCard/emoji/emojiBut
 import KebabButton from '@/components/button/kebab/kebabButton';
 import { CommunityCardProps } from '@/types/communityCardType';
 import NoProfileImg from '@/public/icons/Noprofile.svg';
+import { useState } from 'react';
+import AlertModal from '@/components/modal/alertModal';
+import AddCommunityCard from '@/components/modal/addCommunityCard';
 
 function CommunityCard({
   communityId,
@@ -14,6 +17,16 @@ function CommunityCard({
   review,
   kebab = false,
 }: CommunityCardProps) {
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [isAlertModalOpen, setIsAlertModalOpen] = useState(false);
+
+  const handleEditModalOpenClick = () => {
+    setIsEditModalOpen(!isEditModalOpen);
+  };
+
+  const handleAlertModalOpenClick = () => {
+    setIsAlertModalOpen(!isAlertModalOpen);
+  };
   return (
     <div
       className="relative flex h-439 w-347 flex-col gap-20 rounded-[10px] border-[1px]
@@ -26,9 +39,16 @@ function CommunityCard({
           <p className="text-14 font-bold text-gray-4">{userNickname}</p>
           <p className="text-12 font-normal text-gray-3">{createAt}</p>
         </div>
-        {kebab && <KebabButton title1="수정하기" title2="삭제하기" id={communityId} />}
+        {kebab && (
+          <KebabButton
+            title1="수정하기"
+            title2="삭제하기"
+            onClickTitle1={handleEditModalOpenClick}
+            onClickTitle2={handleAlertModalOpenClick}
+          />
+        )}
       </div>
-      <div className="flex-center h-[180px] w-full bg-gray-5 overflow-hidden">
+      <div className="flex-center h-[180px] w-full overflow-hidden bg-gray-5">
         <Image
           src={bookCover}
           alt="책표지"
@@ -40,10 +60,23 @@ function CommunityCard({
         />
       </div>
       <div className="flex flex-col px-20">
-        <p className="text-16 font-bold text-gray-4 h-23 truncate">{bookTitle}</p>
+        <p className="h-23 truncate text-16 font-bold text-gray-4">
+          {bookTitle}
+        </p>
         <p className="h-60 text-14 font-light text-gray-3">{review}</p>
       </div>
       <EmojiButtonContainer />
+      {isEditModalOpen && (
+        <AddCommunityCard onClick={handleEditModalOpenClick} />
+      )}
+      {isAlertModalOpen && (
+        <AlertModal
+          title="정말 삭제하시겠습니까?"
+          description="삭제한 글은 복구할 수 없습니다."
+          onClick={handleAlertModalOpenClick}
+          id={communityId}
+        />
+      )}
     </div>
   );
 }
