b9a7208656589d23543c5cf166dbd7370c35a004
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-02-22 04:40:07 +0900

    fix : refundmodal 주석, 배포에러 수정

--- src/components/modal/getRefund/index.tsx
+++ src/components/modal/getRefund/index.tsx
@@ -1,13 +1,13 @@
 import { OnClickProps } from '@/types/onClickType';
 import ModalContainer from '@/components/modal/modalContainer';
 import ModalLayout from '@/components/modal/modalLayout';
-import GetRefundForm from '@/components/modal/getRefund/getRefundForm';
+// import GetRefundForm from '@/components/modal/getRefund/getRefundForm';
 
 function GetRefund({ onClick }: OnClickProps) {
   return (
     <ModalLayout onClick={onClick}>
       <ModalContainer onClick={onClick} title="교환/환불 신청하기">
-        <GetRefundForm />
+        {/* <GetRefundForm /> */}
       </ModalContainer>
     </ModalLayout>
   );
