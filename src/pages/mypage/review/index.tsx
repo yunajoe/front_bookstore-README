ccc68b6d32d565d6ffe8bc8274f1666c5a034f0f
HellenHong <hongjw030@naver.com>
2024-02-21 15:21:30 +0900

    feat: 리뷰가 없을 때 보일 레이아웃 생성

--- src/pages/mypage/review/index.tsx
+++ src/pages/mypage/review/index.tsx
@@ -6,10 +6,17 @@ import { myReviews } from '@/pages/api/mock/myReviewMock';
 export default function MyReviewPage() {
   const { data, isLoading, isError } = useGetMyReviewList(1);
   console.log(data);
+  let reviewList = data ?? [];
   return (
     <MainLayout>
       <div className="mx-auto flex w-full flex-col justify-center px-60 py-20 mobile:px-15 mobile:py-0 tablet:px-40">
-        <MyReviewCardList myReviewData={myReviews} />
+        {data && data.length > 0 ? (
+          <MyReviewCardList myReviewData={myReviews} />
+        ) : (
+          <div className="flex-center pt-120 mobile:pt-80">
+            작성한 리뷰가 없어요!
+          </div>
+        )}
       </div>
     </MainLayout>
   );
