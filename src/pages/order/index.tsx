a4287d4220f481fd8126add70e7d52ff389cfae4
thisisthewa2 <thisisthewa2@gmail.com>
2024-02-21 11:57:11 +0900

    fix: 불필요한 코드 삭제

--- src/pages/order/index.tsx
+++ src/pages/order/index.tsx
@@ -7,7 +7,7 @@ import TotalPriceCard from '@/components/card/totalPaymentCard';
 import Link from 'next/link';
 import { basketItemList } from '@/store/state';
 import { useAtomValue } from 'jotai';
-import PaymentButton from '@/components/button/payment/paymentButton';
+
 export default function Order() {
   const items = useAtomValue(basketItemList);
 
@@ -27,7 +27,9 @@ export default function Order() {
         <Link
           className="flex-center sticky bottom-0 h-70 w-full border-t border-gray-1 bg-white pc:hidden"
           href="/paymented">
-          <PaymentButton />
+          <div className="mx-40 flex w-full">
+            <RegisterButton>결제하기</RegisterButton>
+          </div>
         </Link>
       </div>
     </div>
