c87451874e76ac07caaeccc00a94f4346f972fc6
HellenHong <hongjw030@naver.com>
2024-02-21 15:28:12 +0900

    feat: 스켈레톤 ui 추가

--- src/pages/mypage/review/index.tsx
+++ src/pages/mypage/review/index.tsx
@@ -1,16 +1,23 @@
 import { useGetMyReviewList } from '@/api/member';
 import MyReviewCardList from '@/components/card/bookReviewCard/myReviewCardList';
 import MainLayout from '@/components/layout/mainLayout';
+import SkeletonBookOverviewCard from '@/components/skeleton/bookOverviewCard/skeleton';
 import { myReviews } from '@/pages/api/mock/myReviewMock';
 
 export default function MyReviewPage() {
-  const { data, isLoading, isError } = useGetMyReviewList(1);
-  console.log(data);
+  const { data, isLoading } = useGetMyReviewList(1);
   let reviewList = data ?? [];
+
   return (
     <MainLayout>
       <div className="mx-auto flex w-full flex-col justify-center px-60 py-20 mobile:px-15 mobile:py-0 tablet:px-40">
-        {data && data.length > 0 ? (
+        {isLoading ? (
+          Array.from({
+            length: 5,
+          }).map((_, index) => (
+            <SkeletonBookOverviewCard key={index} size="sm" />
+          ))
+        ) : reviewList && reviewList?.length > 0 ? (
           <MyReviewCardList myReviewData={myReviews} />
         ) : (
           <div className="flex-center pt-120 mobile:pt-80">
