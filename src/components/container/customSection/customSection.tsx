5e19df69bebc4052d1189a439314b11a1151ef17
thisisthewa2 <thisisthewa2@gmail.com>
2024-02-23 19:34:40 +0900

    style: 여백 및 간격 수정

--- src/components/container/customSection/customSection.tsx
+++ src/components/container/customSection/customSection.tsx
@@ -42,11 +42,10 @@ function CustomSection() {
   const getRandomCustomBookList = useQuery({
     queryKey: ['randomBook', selectedGenreId],
     queryFn: () => getRandomBookList(selectedCategoryIds, true),
-    enabled: initialDataLoaded && !!selectedCategoryIds, // 초기 데이터가 로드되고 선택된 카테고리가 있는 경우에만 활성화
+    enabled: initialDataLoaded && !!selectedCategoryIds,
     select: (data) => filteredBooks(data.data.books),
   });
 
-  // 랜덤 3개 선택 위함
   useEffect(() => {
     try {
       if (
@@ -54,7 +53,7 @@ function CustomSection() {
         !selectedGenreId
       ) {
         setSelectedGenreId(genreList[0].categoryId);
-        setInitialDataLoaded(true); // 초기 데이터를 로드했음을 표시
+        setInitialDataLoaded(true);
       }
     } catch (error) {
       setSelectedGenreId(null);
@@ -63,7 +62,7 @@ function CustomSection() {
 
   return (
     <div className="flex-center w-full bg-pink">
-      <div className="flex-center w-full max-w-[1200px] mobile:h-[940px] mobile:flex-col  tablet:h-[655px] tablet:flex-col pc:flex pc:h-500 pc:items-center">
+      <div className="flex-center w-full max-w-[1200px] mobile:h-[940px] mobile:flex-col tablet:h-[655px] tablet:flex-col pc:flex pc:h-500 pc:items-center">
         <GenreSelection
           genreList={genreList}
           selectedGenreId={selectedGenreId}
@@ -73,51 +72,49 @@ function CustomSection() {
             ?.length === 0 ? (
           <VacantCustomLayout />
         ) : (
-          <div className="mobile:flex-center flex flex-wrap  gap-x-30 mobile:mx-15 mobile:mb-60  mobile:mt-40 mobile:w-360 mobile:gap-x-10 mobile:gap-y-35 mobile:pr-15 tablet:mt-63 tablet:gap-x-20 pc:ml-75 pc:mr-60 pc:justify-between">
+          <div className="mobile:flex-center relative flex  flex-wrap gap-x-30 mobile:mx-15  mobile:mb-60 mobile:mt-40 mobile:w-360 mobile:gap-x-10 mobile:gap-y-35 mobile:pr-15 tablet:mt-63 tablet:gap-x-20 pc:ml-75 pc:mr-60 pc:justify-between">
             {getRandomCustomBookList.isLoading ? (
               <div></div>
             ) : (
               getRandomCustomBookList.data?.map(
-                (book: CusTomBookType, index: number) => {
-                  return (
-                    <div key={book.bookId}>
-                      {index === 3 && (
-                        <div className="text-right mobile:hidden tablet:hidden">
-                          <Link href="/custom" className="text-primary">
-                            더보기
-                          </Link>
-                        </div>
-                      )}
-                      {index === 1 && (
-                        <div className="text-right tablet:hidden pc:hidden">
-                          <Link href="/custom" className="text-primary">
-                            더보기
-                          </Link>
-                        </div>
-                      )}
-                      <div className="mobile:hidden tablet:hidden">
-                        <PreviewBookInfo
-                          size="md"
-                          bookId={book.bookId}
-                          image={book.bookImgUrl}
-                          title={book.bookTitle}
-                          authorList={book.authors}
-                          isUnit={true}
-                        />
+                (book: CusTomBookType, index: number) => (
+                  <div key={book.bookId} className="mt-17">
+                    {index === 3 && (
+                      <div className="absolute right-3 top-0 mobile:hidden">
+                        <Link href="/custom" className="text-primary">
+                          더보기
+                        </Link>
                       </div>
-                      <div className=" pc:hidden">
-                        <PreviewBookInfo
-                          size="lg"
-                          bookId={book.bookId}
-                          image={book.bookImgUrl}
-                          title={book.bookTitle}
-                          authorList={book.authors}
-                          isUnit={true}
-                        />
+                    )}
+                    {index === 1 && (
+                      <div className="absolute right-0 top-0 tablet:hidden pc:hidden">
+                        <Link href="/custom" className="text-primary">
+                          더보기
+                        </Link>
                       </div>
+                    )}
+                    <div className="mobile:hidden tablet:hidden">
+                      <PreviewBookInfo
+                        size="md"
+                        bookId={book.bookId}
+                        image={book.bookImgUrl}
+                        title={book.bookTitle}
+                        authorList={book.authors}
+                        isUnit={true}
+                      />
                     </div>
-                  );
-                },
+                    <div className=" pc:hidden">
+                      <PreviewBookInfo
+                        size="lg"
+                        bookId={book.bookId}
+                        image={book.bookImgUrl}
+                        title={book.bookTitle}
+                        authorList={book.authors}
+                        isUnit={true}
+                      />
+                    </div>
+                  </div>
+                ),
               )
             )}
           </div>
