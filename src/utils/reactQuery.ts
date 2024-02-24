6c10b3bdd04c3559e5ae678831016fbdf9c132fa
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-02-24 17:13:21 +0900

    fix : enabled 설정 수정

--- src/utils/reactQuery.ts
+++ src/utils/reactQuery.ts
@@ -9,7 +9,7 @@ export const useFetch = <T>(
   const context = useQuery({
     queryKey: [queryKey, params],
     queryFn: () => queryFn(params),
-    enabled: !!enabled ?? true
+    enabled: enabled ? !!`${enabled}` : true,
   });
 
   return context;
