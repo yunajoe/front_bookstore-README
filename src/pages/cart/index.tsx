3b7a63f2f5f448044ebf42e2d5ecf0abef15630a
yunajoe <86518113+yunajoe@users.noreply.github.com>
2024-02-25 22:06:19 +0900

    page에 bottom추가

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -105,8 +105,8 @@ function CartPage() {
             className="flex w-full gap-x-30 px-60 mobile:flex-col mobile:gap-x-10 mobile:px-15 tablet:gap-x-20
                 tablet:px-40">
             <div
-              className="mt-40 grid w-full flex-1 grid-cols-1 gap-x-20 gap-y-20 mobile:mt-20 mobile:grid-cols-1
-                  mobile:gap-y-10 tablet:mt-20 tablet:grid-cols-1">
+              className="mb-120 mt-40 grid w-full flex-1 grid-cols-1 gap-x-20 gap-y-20 mobile:mt-20
+                  mobile:grid-cols-1 mobile:gap-y-10 tablet:mt-20 tablet:grid-cols-1">
               <div className="h-27 text-20 font-bold text-black">
                 장바구니
                 {wishListData &&
