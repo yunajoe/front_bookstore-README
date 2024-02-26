cce962b59f3202bd4a22e84865cacbfbe78b9aa5
HellenHong <hongjw030@naver.com>
2024-02-27 00:16:02 +0900

    FIX

--- src/pages/signin/[socialType].tsx
+++ src/pages/signin/[socialType].tsx
@@ -22,11 +22,12 @@ function SocialPage() {
   });
 
   const handleSocialLogin = async () => {
+    let token = data?.Authentication.substr(7);
     const result = await signIn('social-credentials', {
       email: data?.email,
       socialType: myType,
       memberId: data?.memberId,
-      accessToken: data?.Authentication,
+      accessToken: token,
       redirect: false,
       callbackUrl: '/',
     });
