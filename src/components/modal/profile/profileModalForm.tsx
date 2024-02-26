0d262d7d600c6437809a3c2ac9a50a2cd49bc2bf
hyejungan <138510303+hyejungan@users.noreply.github.com>
2024-02-27 02:31:03 +0900

    feat : 반응형

--- src/components/modal/profile/profileModalForm.tsx
+++ src/components/modal/profile/profileModalForm.tsx
@@ -12,7 +12,7 @@ interface ProfileModalFormProps {
 
 function ProfileModalForm({onClick, profileImage, nickname, email} : ProfileModalFormProps ) {
   return (
-    <div className="relative flex-center px-119 pb-100 pt-60 flex-col">
+    <div className="relative flex-center flex-col px-119 pb-100 pt-60 mobile:px-65 mobile:pb-81 mobile:pt-40">
       <h1 className="text-20 font-bold">프로필 보기</h1>
       <Spacing height={[60, 60, 60]} />
       <div className="relative h-200 w-200 overflow-hidden rounded-full">
