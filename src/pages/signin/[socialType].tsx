75b6e510563f4280d62ff964b574ebd888e5df1f
HellenHong <hongjw030@naver.com>
2024-02-26 23:26:03 +0900

    feat: 소셜로그인 기능

--- src/pages/signin/[socialType].tsx
+++ src/pages/signin/[socialType].tsx
@@ -1,21 +1,44 @@
-import { SocialType, getSocialLogin } from "@/api/social";
-import { useQuery } from "@tanstack/react-query";
-import { useRouter } from "next/router";
+import { SocialType, getSocialLogin } from '@/api/social';
+import { useQuery } from '@tanstack/react-query';
+import { useRouter } from 'next/router';
+import { signIn } from 'next-auth/react';
+import { useEffect } from 'react';
 
 function SocialPage() {
   const router = useRouter();
   const { socialType, code } = router.query;
-  const myType: SocialType = socialType === "kakao" ? "KAKAO" : socialType === "google" ? "GOOGLE" : "NAVER";
-  const { data } = useQuery({
-    queryKey: [""],
-    queryFn: () => getSocialLogin(myType, code as string),
-    enabled: !!socialType
-  })
-  console.log(data);
+  const myType: SocialType =
+    socialType === 'kakao'
+      ? 'KAKAO'
+      : socialType === 'google'
+        ? 'GOOGLE'
+        : 'NAVER';
 
-  return (
-    <div>로그인 중이에요~!! 잠시만용...<></></div>
-  )
+  const { data } = useQuery({
+    queryKey: [''],
+    queryFn: () => getSocialLogin(myType, code as string),
+    enabled: !!socialType,
+    retry: 3,
+  });
+
+  const handleSocialLogin = async () => {
+    const result = await signIn('social-credentials', {
+      email: data?.email,
+      socialType: myType,
+      memberId: data?.memberId,
+      accessToken: data?.Authentication,
+      redirect: false,
+      callbackUrl: '/',
+    });
+    if (result && result?.url && typeof window !== 'undefined') {
+      window.location.href = result.url;
+    }
+  };
+  useEffect(() => {
+    handleSocialLogin();
+  }, []);
+
+  return;
 }
 
 export default SocialPage;
