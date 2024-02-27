c681ed79517791e056afa32af165054c3f5c0b42
orrhrr <120437902+orrhrr@users.noreply.github.com>
2024-02-27 17:48:58 +0900

    refactor: 나의 커뮤니티 글 없을때 UI 추가 (#433)

--- src/components/card/communityCard/communityCardList.tsx
+++ src/components/card/communityCard/communityCardList.tsx
@@ -1,6 +1,6 @@
 import {
   CommunityPagesProps,
-  CommunityCardsProps
+  CommunityCardsProps,
 } from '@/types/api/community';
 import CommunityCard from '@/components/card/communityCard/communityCard';
 
@@ -10,36 +10,46 @@ interface CommunityCardListProps {
   profile?: boolean;
 }
 
-function CommunityCardList({ communityData, kebab, profile}: CommunityCardListProps) {
+function CommunityCardList({
+  communityData,
+  kebab,
+  profile,
+}: CommunityCardListProps) {
   return (
     <>
-      {communityData?.map((data, index) => {
-        return (
-          <div
-            key={index}
-            className="mb-20 grid auto-rows-auto grid-cols-3 gap-20 mobile:grid-cols-1 tablet:grid-cols-2">
-            {data?.cards.map((card: CommunityCardsProps, index) => {
-              return (
-                <CommunityCard
-                  key={index}
-                  communityId={card.communityId}
-                  profileImg={card?.writer?.profileImg}
-                  userNickname={card?.writer.nickname}
-                  createAt={card?.createDate.split('T')[0].replace(/-/g, '.')}
-                  bookId={card?.bookInfo.bookId}
-                  bookCover={card?.bookInfo.bookImgUrl}
-                  bookTitle={card?.bookInfo.bookTitle}
-                  review={card?.content}
-                  emojiInfo={card?.emojiInfo}
-                  writer={card.writer}
-                  kebab={kebab}
-                  profile={profile}
-                />
-              );
-            })}
-          </div>
-        );
-      })}
+      {communityData && communityData[0].cards.length > 0 ? (
+        communityData?.map((data, index) => {
+          return (
+            <div
+              key={index}
+              className="mb-20 grid auto-rows-auto grid-cols-3 gap-20 mobile:grid-cols-1 tablet:grid-cols-2">
+              {data?.cards.map((card: CommunityCardsProps, index) => {
+                return (
+                  <CommunityCard
+                    key={index}
+                    communityId={card.communityId}
+                    profileImg={card?.writer?.profileImg}
+                    userNickname={card?.writer.nickname}
+                    createAt={card?.createDate.split('T')[0].replace(/-/g, '.')}
+                    bookId={card?.bookInfo.bookId}
+                    bookCover={card?.bookInfo.bookImgUrl}
+                    bookTitle={card?.bookInfo.bookTitle}
+                    review={card?.content}
+                    emojiInfo={card?.emojiInfo}
+                    writer={card.writer}
+                    kebab={kebab}
+                    profile={profile}
+                  />
+                );
+              })}
+            </div>
+          );
+        })
+      ) : (
+        <div className="flex-center pt-100 mobile:pt-60">
+          작성한 커뮤니티글이 없어요!
+        </div>
+      )}
     </>
   );
 }
