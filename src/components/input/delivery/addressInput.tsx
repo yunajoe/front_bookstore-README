4a432efdc5d445f056362d978682abb919a4ff1f
thisisthewa2 <thisisthewa2@gmail.com>
2024-02-27 15:35:05 +0900

    fix: addressinput type 수정

--- src/components/input/delivery/addressInput.tsx
+++ src/components/input/delivery/addressInput.tsx
@@ -7,7 +7,6 @@ import AddressLabel from './addressLabel';
 import AddressInputField from './addressInputField';
 import AddressTextArea from './addressTextArea';
 import { deliveryInfoAtom } from '@/store/deliveryInfo';
-import { useGetMember } from '@/api/member';
 // AddressInput 컴포넌트
 interface AddressInputProps {
   isDefault: boolean;
@@ -20,6 +19,8 @@ function AddressInput({ isDefault, addressLines }: AddressInputProps) {
   const [isFindAddressModalOpen, setIsFindAddressModalOpen] = useState(false);
   const [customAddressLine, setCustomAddressLine] = useState('');
   const [, setDeliveryInfo] = useAtom(deliveryInfoAtom);
+
+  const addressLine = addressLines ? addressLines : [];
 
   const handleFindAddressModalOpen = () => {
     setIsFindAddressModalOpen(!isFindAddressModalOpen);
@@ -41,18 +42,18 @@ function AddressInput({ isDefault, addressLines }: AddressInputProps) {
       <AddressLabel>주소</AddressLabel>
       <div className="flex w-full flex-col gap-y-12">
         <div className="flex items-center">
-          <AddressInputField value={isDefault ? addressLines[0] : zipNo} />
+          <AddressInputField value={isDefault ? addressLine[0] : zipNo} />
           <SearchAddressButton onClick={handleFindAddressModalOpen} />
           {!isDefault && isFindAddressModalOpen && (
             <FindAddress onClick={handleFindAddressModalOpen} />
           )}
         </div>
-        <AddressTextArea value={isDefault ? addressLines[1] : roadAddr} />
+        <AddressTextArea value={isDefault ? addressLine[1] : roadAddr} />
         <input
           type="text"
           id="addressLine3"
           placeholder={`${isDefault ? '' : '상세주소 입력'}`}
-          value={isDefault ? addressLines[2] : customAddressLine}
+          value={isDefault ? addressLine[2] : customAddressLine}
           className={`mt-2 flex h-48 w-full items-center overflow-scroll rounded-[5px] border border-gray-1 bg-gray-5 pl-12 text-16 text-gray-2 ${
             isDefault ? 'pointer-events-none' : 'bg-white'
           }`}
