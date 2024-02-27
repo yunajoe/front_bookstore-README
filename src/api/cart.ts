2b5603db888874c091c29c4cd2fd035a820f6abf
thisisthewa2 <thisisthewa2@gmail.com>
2024-02-28 03:12:02 +0900

    fix: vercel error 해결

--- src/api/cart.ts
+++ src/api/cart.ts
@@ -2,13 +2,14 @@ import { instance } from 'src/libs/instance';
 
 // 로그인 한 회원의 등록한 장바구니 목록 가져오기 API
 export const getBasketList = async () => {
-  const result = await instance.get("basket");
+  const result = await instance.get('basket');
   return result.data;
 };
 
-
 // 해당 user의 장바구니에 들어있는 아이템 삭제
-export const deleteBasketItem = async (basketIds:string) => {
-  const result = await instance.delete(`basket?basketIds=${encodeURI(basketIds)}`);  
-  return result.data
+export const deleteBasketItem = async (basketIds: string) => {
+  const result = await instance.delete(
+    `basket?basketIds=${encodeURI(basketIds)}`,
+  );
+  return result.data;
 };
